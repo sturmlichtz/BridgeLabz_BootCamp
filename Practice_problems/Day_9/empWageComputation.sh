5694d6ea563e68df23eae0d2bc2111a513883de3
sturmlichtz <roshanrbhoir.gmail.com>
2020-07-17 13:20:19 +0530

    added a new function to calculate working hours of employee

--- Practice_problems/Day_9/empWageComputation.sh
+++ Practice_problems/Day_9/empWageComputation.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 welcome_Message="Welcome to Employee wage computation program"
 echo $welcome_Message
 wagePerHour=20
@@ -12,24 +12,36 @@ totalAttendedDays=0
 isAbsent=0
 isPresent=1
 isPartTime=2
+function getWorkHours() {
+	case $attendance in
+	1)
+	totalAttendedHours=$(($totalAttendedHours+8))
+	;;
+	2)
+	totalAttendedHours=$(($totalAttendedHours+4))
+	;;
+	3)
+	totalAttendedHours=$(($totalAttendedHours+0))
+	;;
+	esac
+}
 while [ $totalAttendedDays -le 20 -o $totalAttendedHours -le 100 ]
 do
 	attendance=$((RANDOM%3))
 	case $attendance in
 	$isPresent)
-	totalAttendedHours=$(($totalAttendedHours+8))
+	getWorkHours
 	totalAttendedDays=$(($totalAttendedDays+1))
 	;;
 	$isPartTime)
-	totalAttendedHours=$(($totalAttendedHours+4))
+	getWorkHours
 	totalAttendedDays=$(($totalAttendedDays+1))
 	;;
 	$isAbsent)
-	totalAttendedHours=$(($totalAttendedHours+0))
+	getWorkHours
 	totalAttendedDays=$(($totalAttendedDays+0))
 	;;
 	esac
 done
 monthlyWage=$(($totalAttendedHours*$wagePerHour))
 echo "Monthly Wage of employee is :- "$monthlyWage
-
