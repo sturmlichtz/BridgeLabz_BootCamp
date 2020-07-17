4ee5c6d926ae31d41980d33336fcaac2a9633c72
sturmlichtz <roshanrbhoir.gmail.com>
2020-07-17 10:45:34 +0530

    a feature to calculate daily wage is added

--- Practice_problems/Day_9/empWageComputation.sh
+++ Practice_problems/Day_9/empWageComputation.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 welcome_Message="Welcome to Employee wage computation program"
 echo $welcome_Message
+wagePerHour=20
+fullDayHours=8
+dailyWage=0
 isPresent=1
 isAbsent=0
 attendance=$((RANDOM%2))
 if [ $attendance -eq $isPresent ]
 then
 	echo "Employee is Present"
+	dailyWage=$(($wagePerHour*$fullDayHours))
+	echo "Daily Wages are :- " $dailyWage
 elif [ $attendance -eq $isAbsent ]
 then
 	echo "Empolyee is Absent"
+	echo "Daily Wages are :- " $dailyWage
 fi
+
