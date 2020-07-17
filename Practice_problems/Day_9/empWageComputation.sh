90323e4eff077aff30291e0baced221757d1adb7
sturmlichtz <roshanrbhoir.gmail.com>
2020-07-17 10:23:12 +0530

    added a feature to Check Employee is present or absent

--- Practice_problems/Day_9/empWageComputation.sh
+++ Practice_problems/Day_9/empWageComputation.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 welcome_Message="Welcome to Employee wage computation program"
 echo $welcome_Message
-
+isPresent=1
+isAbsent=0
+attendance=$((RANDOM%2))
+if [ $attendance -eq $isPresent ]
+then
+	echo "Employee is Present"
+elif [ $attendance -eq $isAbsent ]
+then
+	echo "Empolyee is Absent"
+fi
