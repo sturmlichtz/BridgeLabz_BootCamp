22f966ea82a7a9893e988207bd5130aa0db59296
sturmlichtz <roshanrbhoir.gmail.com>
2020-07-17 11:26:32 +0530

    a feature is added to calculate monthly wage

--- Practice_problems/Day_9/empWageComputation.sh
+++ Practice_problems/Day_9/empWageComputation.sh
@@ -5,24 +5,26 @@ wagePerHour=20
 fullDayHours=8
 partTimeHours=4
 dailyWage=0
+monthlyWage=0
+workingDayInMonth=20
 isAbsent=0
 isPresent=1
 isPartTime=2
 attendance=$((RANDOM%3))
 case $attendance in
 $isPresent)
-	echo "Employee was Present for $fullDayHours hours"
-	dailyWage=$(($wagePerHour*$fullDayHours))
-	echo "Daily Wages are :- " $dailyWage
+	echo "Employee was Present for $(($fullDayHours*$workingDayInMonth)) hours"
+	monthlyWage=$(($wagePerHour*$fullDayHours*$workingDayInMonth))
+	echo "Monthly Wage is :- " $monthlyWage
 	;;
 $isPartTime)
-	echo "Employee was Present for $partTimeHours hours"
-	dailyWage=$(($wagePerHour*$partTimeHours))
-	echo "Daily Wages are :- " $dailyWage
+	echo "Employee was Present for $(($partTimeHours*$workingDayInMonth)) hours"
+	monthlyWage=$(($wagePerHour*$partTimeHours*$workingDayInMonth))
+	echo "Monthly Wage is :- " $monthlyWage
 	;;
 $isAbsent)
 	echo "Empolyee was Absent"
-	echo "Daily Wages are :- " $dailyWage
+	echo "Monthly Wage is :- " $monthlyWage
 	;;
 esac
 
